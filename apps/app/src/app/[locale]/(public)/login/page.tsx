d1180451f5ccd04c9e3caa53e70d3d1c30365925
Akuya Ekorot <akuyaekorot@gmail.com>
2024-09-21 18:22:35 +0300

    adopt rose pine main and dawn themes

--- apps/app/src/app/[locale]/(public)/login/page.tsx
+++ apps/app/src/app/[locale]/(public)/login/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
       <p className="text-3xl absolute top-4 left-8 font-semibold">penny.</p>
       <div className="flex flex-col gap-6 grow max-w-md">
         <h1 className="text-4xl font-semibold">Login to penny.</h1>
-        <p className="text-2xl text-muted-foreground">
+        <p className="text-2xl text-secondary-foreground">
           Take control of your finances, stay organized and have a fulfilling
           life.
         </p>
